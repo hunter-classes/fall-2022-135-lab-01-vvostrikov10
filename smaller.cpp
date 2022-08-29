5820f58521f00edbbea6454bc99c428d6de43d2d
vvostrikov10 <v.a.vostrikov10@gmail.com>
2022-08-29 11:18:03 -0400

    finished task A

--- smaller.cpp
+++ smaller.cpp
@@ -1,6 +1,26 @@
-##include <iostream>
+/*
+Author: Vladislav Vostrikov
+Course: CSCI-135
+Instructor: Michael Zamansky
+Assignment: Lab1 smaller.cpp
 
+Here, briefly, at least in one or a few sentences
+describe what the program does.
+*/
+
+#include <iostream>
 int main(){
-  std::cout << "Hello world!\n"
+  int n1, n2;
+  std::cout << "Enter the first number: ";
+  std::cin >> n1;
+  std::cout << "Enter the second number: ";
+  std::cin >> n2;
+  if (n2<n1) {
+    std::cout << "\nThe smaller of the two is " << n2 << std::endl;
+  }
+  else{
+    std::cout << "\nThe smaller of the two is " << n1 << std::endl;
+  }
+
   return 0;
 }
